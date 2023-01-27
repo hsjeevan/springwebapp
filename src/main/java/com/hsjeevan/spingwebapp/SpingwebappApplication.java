ee57077d9f4f501ce38eb4ec62c195c98da09e92
Jeevan <hsjeevan07@gmail.com>
2023-01-26 22:18:18 -0500

    initial commit

---
+++ src/main/java/com/hsjeevan/spingwebapp/SpingwebappApplication.java
@@ -0,0 +1,13 @@
+package com.hsjeevan.spingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsjeevan/spingwebapp/SpingwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsjeevan.spingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
